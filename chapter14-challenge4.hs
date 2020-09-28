a2e6abbffd8d3b360aa6aeb3479552edbc6a4385
Even Brenden <evenbrenden@gmail.com>
2020-09-28 23:55:13 +0200

    Chapter 14 challenges

---
+++ chapter14-challenge4.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE TypeFamilies #-}
+
+class Representable f where
+  type Rep f :: *
+  tabulate :: (Rep f -> x) -> f x
+  index :: f x -> Rep f -> x
+
+data Stream x = Cons x (Stream x) deriving Show
+
+instance Representable Stream where
+  type Rep Stream = Int
+  tabulate f = Cons (f 0) (tabulate (f . (+1)))
+  index (Cons b bs) n = if n == 0 then b else index bs (n - 1)
+
+square :: Int -> Int
+square x = x * x
+
+memo :: Stream Int
+memo = tabulate square
+
+memoSquares :: [Int]
+memoSquares = fmap (index memo) [1..4]
+
+main = do
+  putStrLn $ show memoSquares
+  return ()
