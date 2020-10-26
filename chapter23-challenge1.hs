1090785de295ab69ded9558787e28475aa36de19
Even Brenden <evenbrenden@gmail.com>
2020-10-26 21:49:02 +0100

    Chapter 23 challenge 1: Try Stream

--- chapter23-challenge1.hs
+++ chapter23-challenge1.hs
@@ -19,38 +19,80 @@ instance Comonad (Store s) where
 experiment :: Functor f => (s -> f s) -> Store s a -> f a
 experiment k (Store f s) = f <$> k s
 
+runStore :: Store s a -> a
+runStore (Store sa s) = sa s
+
+-- Stream comonad
+
+data Stream a = Cons a (Stream a) deriving Show
+
+instance Functor Stream where
+  fmap f (Cons a s) = Cons (f a) (fmap f s)
+
+instance Comonad Stream where
+  extract (Cons a _) = a
+  duplicate (Cons a as) = Cons (Cons a as) (duplicate as)
+
+head' :: Stream a -> a
+head' = extract
+
+tail' :: Stream a -> Stream a
+tail' (Cons _ s) = s
+
+get :: Int -> Stream a -> a
+get pos s = if pos == 0 then (head' s) else get (pos - 1) (tail' s)
+
 -- Game of Life
 
-type Grid = [Bool]
+data Cell = Live | Dead deriving Show
 
-get index grid = grid !! (mod index $ length grid)
+type Grid = Stream Cell
 
-next True True True = True
-next False True True = False
-next True True False = False
-next False True False = True
-next True False True = False
-next False False True = True
-next True False False = True
-next False False False = False
+next Live Live Live = Live
+next Dead Live Live = Dead
+next Live Live Dead = Dead
+next Dead Live Dead = Live
+next Live Dead Live = Dead
+next Dead Dead Live = Live
+next Live Dead Dead = Live
+next Dead Dead Dead = Dead
 
-nextValue grid i = next (get (i-1) grid) (get i grid) (get (i+1) grid)
+nextValue grid 0 = next Dead (get 1 grid) (get 2 grid) -- unidirectional so wtd
+nextValue grid pos = next (get (pos - 1) grid) (get pos grid) (get (pos + 1) grid)
 
-gridStore :: Grid -> Store Int Bool
-gridStore grid = Store (\i -> nextValue grid i) 0
+gridStore :: Grid -> Store Int Cell
+gridStore grid =
+  let startPos = 0
+  in Store (\i -> nextValue grid i) startPos
+
+monotonicallyIncreasing :: Int -> Stream Int
+monotonicallyIncreasing a = Cons a (monotonicallyIncreasing (a + 1))
 
 iterations :: Grid -> [Grid]
-iterations state =
-  let store = gridStore state
-      positions i = [i..(length state)-1]
+iterations grid =
+  let store = gridStore grid
+      positions = \pos -> monotonicallyIncreasing pos
       nextIteration = experiment positions store
   in nextIteration:(iterations nextIteration)
 
-initial :: Grid
-initial = [False, True, True, False, False, True, False, True]
+allDead :: Grid
+allDead = Cons Dead allDead
 
-fourFirstIterations = take 4 $ iterations initial
+initial :: Grid
+initial = Cons Live allDead
+
+it1 = head $ iterations initial
+it2 = head $ drop 1 $ iterations initial
+it3 = head $ drop 2 $ iterations initial
+it4 = head $ drop 3 $ iterations initial
+
+toList :: Int -> Stream a -> [a]
+toList n (Cons a s) = if n == 1 then [a] else a:(toList (n-1) s)
 
 main = do
-  putStrLn $ show $ fourFirstIterations
+  putStrLn $ "Initial state:\t" ++ (show $ toList 8 initial)
+  putStrLn $ "1st iteration:\t" ++ (show $ toList 8 it1)
+  putStrLn $ "2nd iteration:\t" ++ (show $ toList 8 it2)
+  putStrLn $ "3rd iteration:\t" ++ (show $ toList 8 it3)
+  putStrLn $ "4th iteration:\t" ++ (show $ toList 8 it4)
   return ()
