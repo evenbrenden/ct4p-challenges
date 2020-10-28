f1e38988de6f7ca8095b0ba7362d24bd0bc41469
Even Brenden <evenbrenden@gmail.com>
2020-10-28 23:29:10 +0100

    Chapter 23 challenge 1: Works 2D

--- chapter23-challenge1.hs
+++ chapter23-challenge1.hs
@@ -1,4 +1,4 @@
--- Store comonad
+-- Store comonad (from Control.Comonad.Store)
 
 class Functor w => Comonad w where
   duplicate :: w a -> w (w a)
@@ -19,62 +19,68 @@ instance Comonad (Store s) where
 experiment :: Functor f => (s -> f s) -> Store s a -> f a
 experiment k (Store f s) = f <$> k s
 
-runStore :: Store s a -> a
-runStore (Store sa s) = sa s
+-- Helpers (from Data.List.Split)
 
-store :: (s -> a) -> s -> Store s a
-store f s = Store f s
+build :: ((a -> [a] -> [a]) -> [a] -> [a]) -> [a]
+build g = g (:) []
 
--- 1-dimensional GoL: Cells with exactly one living neighbor change (alive cells become dead, dead cells become alive)
+chunksOf :: Int -> [e] -> [[e]]
+chunksOf i ls = map (take i) (build (splitter ls)) where
+  splitter :: [e] -> ([e] -> a -> a) -> a -> a
+  splitter [] _ n = n
+  splitter l c n  = l `c` splitter (drop i l) c n
 
-data Cell = Live | Dead deriving Show
-type Position = Int
+-- Game of Life
+
+data Cell = Live | Dead deriving (Eq, Show)
+type Position = (Int, Int)
 type Grid = Store Position Cell
 type Stepper = Grid -> Grid
 
-next Live Live Live = Live
-next Dead Live Live = Dead
-next Live Live Dead = Dead
-next Dead Live Dead = Live
-next Live Dead Live = Dead
-next Dead Dead Live = Live
-next Live Dead Dead = Live
-next Dead Dead Dead = Dead
-
-get :: Grid -> Position -> Cell
-get (Store sa s) pos = sa (s + pos)
-
 calc :: Grid -> Cell
 calc grid =
-  let
-    current = extract grid
-    left = get grid 1
-    right = get grid (-1)
-  in next left current right
+  let current = extract grid
+      relativeNeighbourPositions = [(x, y) | x <- [-1, 0, 1], y <- [-1, 0, 1], (x, y) /= (0, 0)]
+      shiftPositions (x, y) (shiftX, shiftY) = (x + shiftX, y + shiftY)
+      absoluteNeighbourPositions position = shiftPositions position <$> relativeNeighbourPositions
+      neighbours = experiment absoluteNeighbourPositions grid
+      numNeighboursAlive = length (filter (== Live) neighbours)
+  in if (current == Live && (numNeighboursAlive == 2 || numNeighboursAlive == 3)) ||
+        (current == Dead && numNeighboursAlive == 3)
+      then Live
+      else Dead
 
 step :: Stepper
 step = extend calc
 
 makeGrid :: [Position] -> Grid
-makeGrid positions =
-  let
-    startingPosition = 0
-    lookupCell pos = if elem pos positions then Live else Dead
-  in store lookupCell startingPosition
+makeGrid livePositions =
+  let startingPosition = (0, 0)
+      lookupCell position = if elem position livePositions then Live else Dead
+  in Store lookupCell startingPosition
 
 makeIterations :: Stepper -> Grid -> [Grid]
-makeIterations step grid =
-  let nextGrid = step grid
-  in nextGrid:makeIterations step nextGrid
+makeIterations step grid = grid:makeIterations step (step grid)
 
-toList :: Grid -> [Cell]
-toList (Store sa _) = sa <$> [0..]
+toString :: Int -> Grid -> [String]
+toString window (Store sa _) =
+  let view = [(y, x) | x <- [0..window - 1], y <- [0..window - 1]]
+      list = sa <$> view
+      rows = chunksOf window list
+      render cells = concat $ show <$> cells
+      addNewLine string = string ++ "\n"
+  in addNewLine . render <$> rows
 
 main = do
-  let printWindow = 8
-  let numIterations = 4
-  let initialGrid = makeGrid [3, 4]
+  let printWindow = 3
+  let numIterations = 3
+  let initialGrid = makeGrid [(0, 1), (1, 1), (2, 1)]
   let iterations = take numIterations $ makeIterations step initialGrid
-  let printable = show $ take printWindow <$> toList <$> iterations
-  putStrLn printable
+  let printable = toString printWindow <$> iterations
+  putStrLn "------------"
+  mapM_ putStrLn $ head printable
+  putStrLn "------------"
+  mapM_ putStrLn $ head $ drop 1 $ printable
+  putStrLn "------------"
+  mapM_ putStrLn $ head $ drop 2 $ printable
   return ()
