d7778368f7d1778e63a1ec97c2cfc1be031792c0
Even Brenden <evenbrenden@gmail.com>
2020-10-28 09:42:57 +0100

    Chapter 23 challenge 1: Rewrite 1D

--- chapter23-challenge1.hs
+++ chapter23-challenge1.hs
@@ -22,38 +22,15 @@ experiment k (Store f s) = f <$> k s
 runStore :: Store s a -> a
 runStore (Store sa s) = sa s
 
--- Stream functor
-
-data Stream a = Cons a (Stream a) (Stream a) deriving Show
-
-instance Functor Stream where
-  fmap f (Cons a sl sr) = Cons (f a) (fmap f sl) (fmap f sr)
-
-head' :: Stream a -> a
-head' (Cons a _ _ ) = a
-
-fwd :: Stream a -> Stream a
-fwd (Cons _ _ sr) = sr
-
-bwd :: Stream a -> Stream a
-bwd (Cons _ sl _) = sl
-
-get :: Int -> Stream a -> a
-get pos s =
-  if pos == 0
-    then (head' s)
-  else if pos > 0
-    then get (pos - 1) (fwd s)
-  else get (pos + 1) (bwd s)
-
-monotonicallyIncreasing :: Int -> Stream Int
-monotonicallyIncreasing a = Cons a (monotonicallyIncreasing (a - 1)) (monotonicallyIncreasing (a + 1))
+store :: (s -> a) -> s -> Store s a
+store f s = Store f s
 
 -- 1-dimensional GoL: Cells with exactly one living neighbor change (alive cells become dead, dead cells become alive)
 
 data Cell = Live | Dead deriving Show
-
-type Grid = Stream Cell
+type Position = Int
+type Grid = Store Position Cell
+type Stepper = Grid -> Grid
 
 next Live Live Live = Live
 next Dead Live Live = Dead
@@ -64,37 +41,40 @@ next Dead Dead Live = Live
 next Live Dead Dead = Live
 next Dead Dead Dead = Dead
 
-nextValue grid pos = next (get (pos - 1) grid) (get pos grid) (get (pos + 1) grid)
-gridStore :: Grid -> Store Int Cell
-gridStore grid =
-  let startPos = 0
-  in Store (\i -> nextValue grid i) startPos
+get :: Grid -> Position -> Cell
+get (Store sa s) pos = sa (s + pos)
 
-iterations :: Grid -> [Grid]
-iterations grid =
-  let store = gridStore grid
-      positions = \pos -> monotonicallyIncreasing pos
-      nextIteration = experiment positions store
-  in nextIteration:(iterations nextIteration)
+calc :: Grid -> Cell
+calc grid =
+  let
+    current = extract grid
+    left = get grid 1
+    right = get grid (-1)
+  in next left current right
 
-allDead :: Grid
-allDead = Cons Dead allDead allDead
+step :: Stepper
+step = extend calc
 
-initial :: Grid
-initial = Cons Dead allDead (Cons Dead allDead (Cons Dead allDead (Cons Live allDead (Cons Live allDead allDead))))
+makeGrid :: [Position] -> Grid
+makeGrid positions =
+  let
+    startingPosition = 0
+    lookupCell pos = if elem pos positions then Live else Dead
+  in store lookupCell startingPosition
 
-it1 = head $ iterations initial
-it2 = head $ drop 1 $ iterations initial
-it3 = head $ drop 2 $ iterations initial
-it4 = head $ drop 3 $ iterations initial
+makeIterations :: Stepper -> Grid -> [Grid]
+makeIterations step grid =
+  let nextGrid = step grid
+  in nextGrid:makeIterations step nextGrid
 
-toListFwd :: Int -> Stream a -> [a]
-toListFwd n (Cons a sr sl) = if n == 1 then [a] else a:(toListFwd (n - 1) sl)
+toList :: Grid -> [Cell]
+toList (Store sa _) = sa <$> [0..]
 
 main = do
-  putStrLn $ "Initial state:\t" ++ (show $ toListFwd 8 initial)
-  putStrLn $ "1st iteration:\t" ++ (show $ toListFwd 8 it1)
-  putStrLn $ "2nd iteration:\t" ++ (show $ toListFwd 8 it2)
-  putStrLn $ "3rd iteration:\t" ++ (show $ toListFwd 8 it3)
-  putStrLn $ "4th iteration:\t" ++ (show $ toListFwd 8 it4)
+  let printWindow = 8
+  let numIterations = 4
+  let initialGrid = makeGrid [3, 4]
+  let iterations = take numIterations $ makeIterations step initialGrid
+  let printable = show $ take printWindow <$> toList <$> iterations
+  putStrLn printable
   return ()
