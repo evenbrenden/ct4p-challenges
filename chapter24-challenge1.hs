36ee68d22ace258d510315dfa2a5b012c2f51eb5
Even Brenden <evenbrenden@gmail.com>
2020-11-01 15:56:52 +0100

    Redo chapter 24 challenge 1
    Got that really wrong.
    
    Anyway, if you're looking for something that handles arithmetic on
    polynomials then here it is.

--- chapter24-challenge1.hs
+++ chapter24-challenge1.hs
@@ -3,51 +3,21 @@
 #! nix-shell -p ghc
 #! nix-shell -i "ghcid -c 'ghci -Wall' -T main"
 
-type Algebra f a = f a -> a
-
-data RingF a = RZero
-             | ROne
-             | RAdd a a
-             | RMul a a
-             | RNeg a
-
 type Poly = [Int]
 
-add :: (Int, Int) -> Int
-add (x, y) = x + y
+calc :: (Int, Int) -> Int -> Double
+calc (coefficient, power) x = (fromIntegral coefficient) * ((fromIntegral x) ** (fromIntegral power))
 
-zipPad :: Poly -> Poly -> [(Int, Int)]
-zipPad (x:xs) (y:ys) = (x,y):zipPad xs ys
-zipPad [] ys = zip (repeat 0) ys
-zipPad xs [] = zip xs (repeat 0)
+indexed :: Poly -> [(Int, Int)]
+indexed xs = zip xs [0..]
 
-zipWithAddPad :: Poly -> Poly -> Poly
-zipWithAddPad xs ys = add <$> zipPad xs ys
+calcs :: Poly -> Int -> [Double]
+calcs poly x = map (flip calc (fromIntegral x)) (indexed poly)
 
-makeShifts :: Poly -> Int -> [Poly]
-makeShifts xs n =
-    if n == 0 then [xs]
-    else makeShifts xs (n - 1) ++ [replicate n 0 ++ xs]
-
-scale :: Int -> Poly -> Poly
-scale x ys = (*x) <$> ys
-
-multiZip :: Poly -> [Poly] -> [Poly]
-multiZip xs yss = zipWith scale xs yss
-
-concatZipAdd :: [Poly] -> Poly
-concatZipAdd (m:ms) = zipWithAddPad m (concatZipAdd ms)
-concatZipAdd [] = []
-
-evalP :: Algebra RingF Poly
-evalP RZero = []
-evalP ROne = [1] -- Hm
-evalP (RAdd xs ys) = zipWithAddPad xs ys
-evalP (RMul xs ys) = concatZipAdd $ multiZip xs (makeShifts ys (length xs))
-evalP (RNeg n) = map (0-) n
+evalPoly :: Poly -> Int -> Double
+evalPoly poly x = sum $ calcs poly x
 
 main :: IO ()
 main = do
-  let binomial = RMul [1, 2] [2, 1]
-  putStrLn $ show $ evalP binomial
+  putStrLn $ show $ evalPoly [-1, 0, 4] 2
   return ()
