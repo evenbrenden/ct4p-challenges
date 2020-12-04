49263a024bc532c7dc79914b62d73b27a7444c99
Even Brenden <evenbrenden@gmail.com>
2020-12-04 11:09:59 +0100

    Update chapter 18 challenge 6

--- chapter18-challenge6.hs
+++ chapter18-challenge6.hs
@@ -1,15 +1,12 @@
--- Definition by natural isomorphism (also inspired by
--- http://danshiebler.com/2018-11-10-category-solutions/). Alternatively (and
--- maybe how the challenge is meant to be solved), one could declare and
--- implement an Adjunction instance with the two types.
+-- (Also inspired by http://danshiebler.com/2018-11-10-category-solutions/)
 
 #! /usr/bin/env nix-shell
 #! nix-shell -p ghcid
 #! nix-shell -p ghc
 #! nix-shell -i "ghcid -c 'ghci -Wall'"
 
-product2Function :: ((z, a) -> b) -> (z -> (a -> b))
-product2Function za_b = \z -> (\a -> za_b (z, a))
+leftAdjunct :: ((z, a) -> b) -> (z -> (a -> b))
+leftAdjunct za_b = \z -> (\a -> za_b (z, a))
 
-function2Product :: (z -> (a -> b)) -> ((z, a) -> b)
-function2Product z_ab = \za -> (z_ab (fst za) (snd za))
+rightAdjunct :: (z -> (a -> b)) -> ((z, a) -> b)
+rightAdjunct z_ab = \za -> (z_ab (fst za) (snd za))
