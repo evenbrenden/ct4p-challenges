61acca283b5c9cd1c809c36153a5211e1b399556
Even Brenden <evenbrenden@gmail.com>
2021-04-28 13:45:25 +0200

    Add Kan extensions examples

---
+++ chapter27-left-kan-extension.hs
@@ -0,0 +1,25 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -p ghcid
+#! nix-shell -p ghc
+#! nix-shell -i "ghcid -c 'ghci -Wall' -T main"
+
+{-# LANGUAGE GADTs #-}
+
+import Data.Functor.Identity
+
+data Lan k d a = forall i. Lan (k i -> a) (d i)
+
+type Exp a b = Lan ((,) a) Identity b
+
+toExp :: (a -> b) -> Exp a b
+toExp f = Lan (f . fst) (Identity ())
+
+fromExp :: Exp a b -> (a -> b)
+fromExp (Lan f (Identity x)) = \a -> f (a, x)
+
+main :: IO ()
+main = do
+    let exp' = fromExp (toExp (==1))
+    putStrLn $ show $ exp' 1
+    putStrLn $ show $ exp' 2
+    return ()
---
+++ chapter27-right-kan-extension.hs
@@ -0,0 +1,38 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -p ghcid
+#! nix-shell -p ghc
+#! nix-shell -i "ghcid -c 'ghci -Wall' -T main"
+
+{-# LANGUAGE RankNTypes #-}
+
+newtype Ran k d a = Ran (forall i. (a -> k i) -> d i)
+
+-- Replacing d with the forgetful functor = identity functor, we can find the free functor from the category of monoids.
+type Lst a = forall i. Monoid i => (a -> i) -> i
+
+toLst :: [a] -> Lst a
+toLst as = \f -> foldMap f as
+
+-- Generate a list using the free functor from the category of monoids.
+fromLst :: Lst a -> [a]
+fromLst f = f (\a -> [a])
+
+newtype Sum a = Sum a deriving Show
+
+instance Num a => Semigroup (Sum a) where
+    (Sum a) <> (Sum b) = Sum (a + b)
+
+instance Num a => Monoid (Sum a) where
+    mempty = Sum 0
+
+main :: IO ()
+main = do
+    -- k is Sum (the forgetful functor under the sum monoid)
+    -- d is the identity functor (could have used Identity)
+    -- a is an integer
+    -- i is an integer
+    let list = fromLst (toLst [1..5])
+        sum = toLst [1..5] $ Sum
+    putStrLn $ show list
+    putStrLn $ show sum
+    return ()
